06a9c5403ea0c16371f443394c1d9ef0c97d8b30
Geovani237 <geovanicsouza7@gmail.com>
2026-08-10 09:11:39 -0300

    feat:estrutura inicial

---
+++ src/main/java/com/geovani237/corrida_de_rua/CorridaDeRuaApplication.java
@@ -0,0 +1,13 @@
+package com.geovani237.corrida_de_rua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorridaDeRuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorridaDeRuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geovani237/corrida_de_rua/CorridaDeRuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geovani237.corrida_de_rua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorridaDeRuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
